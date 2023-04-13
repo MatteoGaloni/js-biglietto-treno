7eb6d45735e015c37ebf54b7c991e5fab214e37f
MatteoGaloni <mtgaloni@gmail.com>
2023-04-13 15:32:58 +0200

    add parseInt to strings

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ let numeroKm = prompt("Quanti chilometri vuoi percorrere?");
 console.log("I chilometri da percorrere sono", numeroKm);
 let etaPasseggero = prompt("Quanti anni hai?");
 console.log("L'età del passggero è", etaPasseggero);
+
+console.log(parseInt(numeroKm));
+console.log(parseInt(etaPasseggero));
+
