19c4b11d6bc21184819959c9f6d190c1c5e1181c
MatteoGaloni <mtgaloni@gmail.com>
2023-04-13 15:17:17 +0200

    add let etaPasseggero/numeroKm

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let numeroKm = prompt("Quanti chilometri vuoi percorrere?");
+console.log("I chilometri da percorrere sono", numeroKm);
+let etaPasseggero = prompt("Quanti anni hai?");
+console.log("L'età del passggero è", etaPasseggero);
