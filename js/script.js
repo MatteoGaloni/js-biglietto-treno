cd5c0d05638f2292bf2bb12db52decab6ac44f76
MatteoGaloni <mtgaloni@gmail.com>
2023-04-13 16:18:08 +0200

    add let prezzoBiglietto/if etaPassegero<18

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
+// *****variabili km e età*****
+
 let numeroKm = prompt("Quanti chilometri vuoi percorrere?");
 console.log("I chilometri da percorrere sono", numeroKm);
 let etaPasseggero = prompt("Quanti anni hai?");
 console.log("L'età del passggero è", etaPasseggero);
 
-console.log(parseInt(numeroKm));
-console.log(parseInt(etaPasseggero));
+// ***Dichiaro la variabile prezzoBiglietto ***
 
+let prezzoBiglietto = console.log(parseInt(numeroKm) * 0.21);
+
+if (parseInt(etaPasseggero) < 18) {
+    console.log("il prezzo del biglietto è di", prezzoBiglietto);
+}
